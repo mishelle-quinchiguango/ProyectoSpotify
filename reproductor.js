984a1ea4cd7b3f0f0334b9e577bda84d2d230745
darlamai <darlynludena963@gmail.com>
2024-02-08 18:56:14 -0500

    clases para lista de reproducción

--- reproductor.js
+++ reproductor.js
@@ -1,44 +1,81 @@
 // Definición de la clase Cancion
 class Cancion {
-    constructor(titulo, artista) {
-        this.titulo = titulo;
+    constructor(nombre, artista,genero) {
+        this.nombre = nombre;
         this.artista = artista;
+        this.genero=genero;
     }
 }
 
 // Definición de la clase Lista
-class Lista {
-    constructor(nombre, cancionInicial) {
-        this.nombre = nombre;
+class Lista_canciones {
+    constructor(nombre_lista, cancionInicial) {
+        this.nombre = nombre_lista;
         this.canciones = []; // Array para almacenar las canciones
         if (cancionInicial) {
             this.addCancion(cancionInicial);
         }
     }
 
-    // Método para añadir una canción a la lista
-    addCancion(cancion) {
-        this.canciones.push(cancion);
-        console.log(`"${cancion.titulo}" de ${cancion.artista} ha sido añadida a la lista.`);
+    buscarCancion(cancion){
+        return this.canciones.some(c => c.nombre ==cancion.nombre && c.artista==cancion.artista && c.genero==cancion.genero);
     }
 
-    // Método para eliminar una canción de la lista
+    addCancion(cancion) {
+
+        if(!this.buscarCancion(cancion)){
+
+            this.canciones.push(cancion)
+            return true
+
+        }
+        else{
+            return false
+        }
+
+    }
+
     removeCancion(cancion) {
-        const index = this.canciones.findIndex(c => c.titulo === cancion.titulo && c.artista === cancion.artista);
+        const index = this.canciones.findIndex(c => c.nombre ==cancion.nombre && c.artista==cancion.artista && c.genero==cancion.genero);
         if (index !== -1) {
             this.canciones.splice(index, 1);
-            console.log(`"${cancion.titulo}" de ${cancion.artista} ha sido eliminada de la lista.`);
+            return true; 
         } else {
-            console.log(`"${cancion.titulo}" de ${cancion.artista} no está en la lista.`);
+            return false;
         }
     }
 }
 
-// Ejemplo de uso
+
+
+class Favoritos extends Lista_canciones{
+    constructor(nombre, cancionInicial){
+        super(nombre, cancionInicial);
+    }
+}
+
+class MyPlaylist extends Lista_canciones{
+    constructor(nombre, cancionInicial){
+        super(nombre, cancionInicial);
+    }
+}
+
+class PlaylistBusqueda extends Lista_canciones{
+    constructor(nombre, cancionInicial){
+        super(nombre, cancionInicial);
+    }
+
+    buscarCancion(nombre, artista, genero){
+        return this.canciones.find(c => c.nombre ==cancion.nombre && c.artista==cancion.artista && c.genero==cancion.genero);
+    }
+}
+
+
+
 const cancion1 = new Cancion("Bohemian Rhapsody", "Queen");
 const cancion2 = new Cancion("Stairway to Heaven", "Led Zeppelin");
 
-const miLista = new Lista("Lista de reproducción", cancion1);
+const miLista = new Lista_canciones("Lista de reproducción", cancion1);
 miLista.addCancion(cancion2);
 
 console.log("Lista de reproducción:");
